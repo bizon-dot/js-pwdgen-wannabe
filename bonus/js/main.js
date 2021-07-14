cc3fe2238b65ae82c770b35ccdec9778ace3627e
robertoAureli <r.aureli@protonmail.com>
2021-07-14 15:40:25 +0200

    Bonus completo

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -1,18 +1,21 @@
-// Dichiaro le variabili
 
-var user_name;
-var user_last_name;
-var user_color;
-var user_pass;
 
-// Popolo le variabili chiedendo i diversi input all'utente
+function weak_pass() {
+    // Dichiaro le variabili
+    var user_name;
+    var user_last_name;
+    var user_color;
+    var user_pass;
 
-user_name = prompt("Inserisci il tuo nome");
-user_last_name = prompt("Inserisci il tuo cognome");
-user_color = prompt("Inserisci il tuo colore preferito");
+    // Popolo le variabili chiedendo i diversi input all'utente
 
-// Concateno gli input per avere la pass
-user_pass = user_name + user_last_name + user_color;
+    user_name = prompt("Inserisci il tuo nome");
+    user_last_name = prompt("Inserisci il tuo cognome");
+    user_color = prompt("Inserisci il tuo colore preferito");
 
-// Stampo il risultato nell'elemento che ha id #pass
-document.getElementById("pass").innerHTML = user_pass;
+    // Concateno gli input per avere la pass
+    user_pass = user_name + user_last_name + user_color + "21";
+
+    // Stampo il risultato nell'elemento che ha id #pass
+    document.getElementById("pass").innerHTML = user_pass;
+}
